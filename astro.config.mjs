928471e9ba443d598cc9a22dfbc6907cb56ab539
David Jaimes <dj@davidjaimes.com>
2023-06-24 13:58:49 -0700

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from "astro/config";
+
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  sitemap: true,
+  site: "https://davidjaimes.com",
+  outDir: "public",
+  publicDir: "static",
+  
+  integrations: [react(), tailwind()]
+});
